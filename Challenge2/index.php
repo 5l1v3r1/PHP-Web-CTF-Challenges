03647e5e9919264cb45ea868fb84a38de57a28d9
Devansh Batham <devanshbatham009@gmail.com>
2019-12-03 01:46:34 +0530

    Create index.php

---
+++ Challenge2/index.php
@@ -0,0 +1,19 @@
+<?php
+$dev = $_GET['number'];
+$flag = "FLAG{YOU-SOLVED-YOUR-SECOND-CHALLENGE}"
+if (isset($dev)) {
+	if (is_numeric($dev)){
+		if (!strpos($dev, ".")){
+			if (strlen($dev) > 6){
+				if ($dev < 99999 && $dev > 90000)
+				die('<div class="alert"> Flag: '.$flag.' </div>');
+				else
+				print '<div class="alert">Oh Oh Think again</div>';
+			} else
+				print '<h1>Wrong Move Champ</h1>';
+		} else
+		print '<h1>Not That hard . keep trying</h1>';	
+	} else
+		print '</h1>Just read the source code once again</h1>';
+}
+?>
