87fc3996b817ab6ad4c5d8cda80ed7825ba352a6
Devansh Batham <devanshbatham009@gmail.com>
2019-12-04 23:46:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,6 @@ CTF STARTS HERE
 <br><br>
 
 challenge 1 : <a href="https://asm0d3us-ctf.herokuapp.com/Challenge1/index.php">Click Here</a>
+<br><br>
+challenge 2 : <a href="https://asm0d3us-ctf.herokuapp.com/Challenge2/index.php">Click Here</a>
+
